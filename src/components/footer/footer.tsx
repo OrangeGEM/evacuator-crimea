58f1da3e1b2343ad71470dc87a0c0a1269ae1e50
Vitaliy <vitalij.volkov@itp-it.com>
2022-07-31 13:57:13 +0300

    Header+Footer+LP done

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,41 @@
+import { Link } from 'react-router-dom';
 import './footer.scss';
 
 export default function Footer() {
 
     return (
-        <div>
-            
+        <div className="footer">
+            <div className="footer-container">
+                <div className="footer-widgets">
+                    <div className="footer-widget">
+                        <div className="widget-nav">
+                            <p className="nav-element">
+                                <Link to="/rates" className="nav-el"> СТОИМОСТЬ </Link>
+                            </p>
+
+                            <p className="nav-element">
+                                <Link to="/cooperation" className="nav-el"> СОТРУДНИЧЕСТВО </Link>
+                            </p>
+
+                            <p className="nav-element">
+                                <Link to="/about" className="nav-el"> О КОМПАНИИ </Link>
+                            </p>
+
+                            <p className="nav-element">
+                                <Link to="/contracts" className="nav-el">  КОНТАКТЫ</Link>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="footer-widget">
+                        <div className="widget-nav">
+                            <p className="nav-element" style={{ color: "#FF6600", paddingLeft: "100px" }}>
+                                +7 (978) 015 14 14
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,12 +7,15 @@ export default function Header() {
     return (
         <header className="header">
             <div className="header-content">
-                <div className="header-title"> Эвакуатор Ялта </div>
+                <div className="header-title">
+                    <Link to="/" className="header-navlink" style={{color:"#FF6600"}}>  Эвакуатор Ялта </Link>
+
+                </div>
                 <nav>
-                    <Link to="/" className="header-navlink"> СТОИМОСТЬ </Link>
-                    <Link to="/" className="header-navlink"> СОТРУДНИЧЕСТВО </Link>
-                    <Link to="/" className="header-navlink"> О КОМПАНИИ </Link>
-                    <Link to="/" className="header-navlink"> КОНТАКТЫ </Link>
+                    <Link to="/rates" className="header-navlink"> СТОИМОСТЬ </Link>
+                    <Link to="/cooperation" className="header-navlink"> СОТРУДНИЧЕСТВО </Link>
+                    <Link to="/about" className="header-navlink"> О КОМПАНИИ </Link>
+                    <Link to="/contracts" className="header-navlink"> КОНТАКТЫ </Link>
                 </nav>
             </div>
         </header>
